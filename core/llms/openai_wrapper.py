29e20ad04fe8e636678100bbcdf29dca509abdbc
bigbrother666 <96130569+bigbrother666sh@users.noreply.github.com>
2025-01-08 09:48:15 +0800

    Update openai_wrapper.py
    to resolve error raise by 'logger is None'
    (This problem existed in the previous version. It was not caused by your code. I just modified it.）
    
    Signed-off-by: bigbrother666 <96130569+bigbrother666sh@users.noreply.github.com>

--- core/llms/openai_wrapper.py
+++ core/llms/openai_wrapper.py
@@ -31,17 +31,25 @@ async def openai_llm(messages: list, model: str, logger=None, **kwargs) -> str:
         response = await client.chat.completions.create(messages=messages, model=model, **kwargs)
         resp = response.choices[0].message.content
     except RateLimitError as e:
-        logger.warning(f'{e}\nRetrying in 60 second...')
+        if logger:
+            logger.warning(f'{e}\nRetrying in 60 second...')
+        else:
+            print(f'{e}\nRetrying in 60 second...')
         await asyncio.sleep(60)
         response = await client.chat.completions.create(messages=messages, model=model, **kwargs)
         if response.status_code == 200 and response.choices:
             resp = response.choices[0].message.content
         else:
-            logger.error(f'after many try, llm error: {response}')
+            if logger:
+                logger.error(f'after many try, llm error: {response}')
+            else:
+                print(f'after many try, llm error: {response}')
 
     except Exception as e:
         if logger:
             logger.error(f'openai_llm error: {e}')
+        else:
+            print(f'openai_llm error: {e}')
 
     finally:
         semaphore.release()
